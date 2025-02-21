11b3ab31dbb638ccd8f4ce0d7dc81e2b682de87d
PreyeO <omusukup@yahoo.com>
2025-02-21 10:01:58 +0100

    feat(coming soon): add coming soon place holder

---
+++ src/page/Landing.jsx
@@ -0,0 +1,12 @@
+const Landing = () => {
+  return (
+    <main>
+      <div>
+        <h1>SERENA BRAIDE</h1>
+        <h3>COMING SOON!!!!</h3>
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
