c69686b745da33ce13e456fb738d66a2e5621e82
PreyeO <omusukup@yahoo.com>
2025-03-24 11:47:24 +0100

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,16 @@ const Hero = () => {
     <section>
       <div className="container">
         <h1 className="">
-          SerenaBraide. <span className="">Luxury without limits.</span>
+          Scents
+          <span className="span-white"> That Speak Without Words.</span>
         </h1>
 
-        <p>Refined. Bold. Uncompromising.</p>
+        <p>
+          Signature scents for those who command presence.
+          <br />
+          <span className="span-color">SERENA BRAIDE</span> - Luxury in every
+          note.
+        </p>
         <h3>LAUNCHING SOON!!!</h3>
       </div>
       <div>
